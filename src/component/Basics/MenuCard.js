ec62bce89cf4c03f2fe75b32c6e139b37fb92950
bhoomihz <bhoomisvshetty@gmail.com>
2023-08-14 19:10:40 +0530

    Frontend task code

---
+++ src/component/Basics/MenuCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const MenuCard = ({ menuData }) => {
+  //   console.log(menuData);
+
+  return (
+    <>
+      <section className="main-card--cointainer">
+        {menuData.map((curElem) => {
+          const { id, name, category, tag} = curElem;
+
+          return (
+            <>
+              <div className="card-container" key={id}>
+                <div className="card ">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtle">{id}</span>
+                    <span className="card-author subtle"> {category}</span>
+                    <h2 className="card-title"> {name} </h2>
+                    <span className="card-author subtle"> {tag}</span>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+export default MenuCard;
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,66 @@
+const Menu = [
+  {
+    id: 1,
+
+    name: "Update User Profile Page UI",
+    category: "Todo",
+    "tag":"Feature Request",
+  },
+
+  {
+    id: 2,
+    name: "Add Multi-Language Support ",
+    category: "In progress",
+    "tag":"Feature Request",
+  },
+  {
+    id: 3,
+    name: "Optimize Database Queries",
+    category: "In progress",
+    "tag":"Feature Request",
+  },
+  {
+    id: 4,
+    name: "Implement Email Notification System",
+    category: "In progress",
+    "tag":"Feature Request",
+  },
+  {
+    id: 5,
+    name: "Enhance Search Functionality",
+    category: "In progress",
+    "tag":"Feature Request",
+  },
+  {
+    id: 6,
+    name: "Third-Party Payment Gateway",
+    category: "Todo",
+    "tag":"Feature Request",
+  },
+  {
+    id: 7,
+    name: "Create Onboarding Tutorial",
+    category: "Backlog",
+    "tag":"Feature Request",
+  },
+  {
+    id: 8,
+    name: "Implement Role-Based Access Control",
+    category: "In progress",
+    "tag":"Feature Request",
+  },
+  {
+    id: 9,
+    name: "Upgrade Server Infrastructure",
+    category: "Todo",
+    "tag":"Feature Request",
+  },
+  {
+    id: 10,
+    name: "Security Vulnerability Assessment",
+    category: "Backlog",
+    "tag":"Feature Request",
+  },
+];
+
+export default Menu;
